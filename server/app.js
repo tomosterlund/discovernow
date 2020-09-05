a2476bc770cafd77fc325bb6a159498b8d6b1114
Tom Österlund <tom.osterlund1@gmail.com>
2020-09-05 09:16:32 +0200

    adds images dir

--- server/app.js
+++ server/app.js
@@ -51,7 +51,7 @@ app.use(session({
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 
 // DB-setup
-const dbUri = '';
+const dbUri = 'mongodb+srv://tommy:tommy123@cluster0.6ne0u.mongodb.net/discover-deployed?retryWrites=true&w=majority';
 mongoose.connect(dbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
